e8e08309c4864e8ac3d4c45b4d7e9c74f4e34d20
naskobogdanov <naskobogdanov@gmail.com>
2014-03-31 23:26:36 +0300

    Just HomeWork 1

---
+++ 8.SquareRoot/SquareRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        Console.WriteLine("Square Root of 12345 is:");
+        Console.WriteLine(12345*12345);
+    }
+}
---
+++ MyAge+10/MyAge+10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class MyAge10
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your birth year below...");
+
+        int startingYear = 1900;
+        int nowadays = DateTime.Now.Year;
+        int birthYear = int.Parse(Console.ReadLine());
+        int currentAge = (nowadays - birthYear);
+        int inTenYears = currentAge + 10;
+
+        
+        if (birthYear >= startingYear && birthYear < nowadays)
+        {
+            Console.WriteLine("You are now " + currentAge + " Years Old");
+
+            Console.WriteLine("10 Years from now you will be: " + inTenYears + " Years Old");
+        }
+        else
+        {
+            Console.WriteLine("Sorry! You either entered a wrong Birth Year or you're too old!");
+        }
+        Console.ReadKey();
+    }
+}
